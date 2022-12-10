9ee0cc335d6badccfb2d69d97efe4381bbe8d37e
Luana <santosmeiraluana@gmail.com>
2022-12-09 23:07:51 -0300

    dando inicio a criação de menu, header e timeline

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "561a2d480b6f5592"; }
+/******/ 		__webpack_require__.h = function() { return "70b187b26f9d6adf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,36 @@
 function HomePage() {
-    return <div>Bem vindo a AluraTube</div>
+    const estiloDasHomePages = { backgroundColor: "red" };
+    return (
+        <div style={estiloDasHomePages} > 
+            <Menu />
+            <Header />
+            <Timeline />
+        </div>
+    )
   }
   
   export default HomePage
+
+  function Menu() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+  }
+
+  function Header() {
+    return (
+        <div>
+            Header
+        </div>
+    )
+  }
+
+  function Timeline() {
+    return (
+        <div>
+            Timeline
+        </div>
+    )
+  }
