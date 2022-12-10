d8e74a7405b429403eaa1b9287949b5a80cddffc
Luana <santosmeiraluana@gmail.com>
2022-12-10 00:05:44 -0300

    adicionando styled components

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import config from "../config.json";
+import styled from "styled-components";
 
 function HomePage() {
     const estiloDasHomePages = { backgroundColor: "red" };
@@ -21,15 +22,31 @@ function HomePage() {
     )
   }
 
+  const StyledHeader = styled.div`
+    img {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
+    user-info {
+        display: flex;
+        align-items: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+  `;
   function Header() {
     return (
-        <div>
-            <img src="banner" />
-            <img src={`https://github.com/${config.github}.png`} />
-            {config.name}
-            {config.job}
+        <StyledHeader>
+            {/*<img src="banner" />*/}
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                {config.name}
+                {config.job}
+            </section>
             
-        </div>
+        </StyledHeader>
     )
   }
 
